234f3106534102f06e57c787b03bb41b177c8ed1
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-08 18:04:21 +0200

    Issue #2 Add (modified) tests from cookiecutter-flask

---
+++ tests/factories.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Factories to help in tests."""
+
+from factory import PostGenerationMethodCall, Sequence
+from factory.alchemy import SQLAlchemyModelFactory
+
+from xl_auth.database import db
+from xl_auth.user.models import User
+
+
+class BaseFactory(SQLAlchemyModelFactory):
+    """Base factory."""
+
+    class Meta:
+        """Factory configuration."""
+
+        abstract = True
+        sqlalchemy_session = db.session
+
+
+class UserFactory(BaseFactory):
+    """User factory."""
+
+    username = Sequence(lambda n: 'user{0}'.format(n))
+    email = Sequence(lambda n: 'user{0}@example.com'.format(n))
+    password = PostGenerationMethodCall('set_password', 'example')
+    active = True
+
+    class Meta:
+        """Factory configuration."""
+
+        model = User
