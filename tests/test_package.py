9c45e454d7d0a8ec6b1de890aa21867d637bc985
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-11 11:11:12 +0200

    Add rewritten package_tests.py from 0.1.0 as test_package.py

---
+++ tests/test_package.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Test :py:mod:`xl_auth` package identifiers."""
+
+from __future__ import (print_function, absolute_import, unicode_literals, division)
+
+import os
+import json
+
+import xl_auth
+
+
+def test_package_name_is_xl_auth():
+    assert xl_auth.__name__ == 'xl_auth'
+
+
+def test_package_version_matches_package_json():
+    with open(os.path.join(os.path.dirname(__file__), '..', 'package.json')) as package_json:
+        assert xl_auth.__version__ == json.load(package_json)['version']
