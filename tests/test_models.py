aea15a47e36cd6aac24b5a051ac9524bab68046b
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-11 10:42:22 +0200

    Refactor tests for User model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Model unit tests."""
+"""Unit tests for User model."""
 
 import datetime as dt
 
@@ -11,65 +11,74 @@ from .factories import UserFactory
 
 
 @pytest.mark.usefixtures('db')
-class TestUser(object):
-    """User tests."""
+def test_get_by_id():
+    """Get user by ID."""
 
-    def test_get_by_id(self):
-        """Get user by ID."""
+    user = User('foo', 'foo@bar.com')
+    user.save()
 
-        user = User('foo', 'foo@bar.com')
-        user.save()
+    retrieved = User.get_by_id(user.id)
+    assert retrieved == user
 
-        retrieved = User.get_by_id(user.id)
-        assert retrieved == user
 
-    def test_created_at_defaults_to_datetime(self):
-        """Test creation date."""
+@pytest.mark.usefixtures('db')
+def test_created_at_defaults_to_datetime():
+    """Test creation date."""
 
-        user = User(username='foo', email='foo@bar.com')
-        user.save()
-        assert bool(user.created_at)
-        assert isinstance(user.created_at, dt.datetime)
+    user = User(username='foo', email='foo@bar.com')
+    user.save()
+    assert bool(user.created_at)
+    assert isinstance(user.created_at, dt.datetime)
 
-    def test_password_is_nullable(self):
-        """Test null password."""
 
-        user = User(username='foo', email='foo@bar.com')
-        user.save()
-        assert user.password is None
+@pytest.mark.usefixtures('db')
+def test_password_is_nullable():
+    """Test null password."""
 
-    def test_factory(self, db):
-        """Test user factory."""
+    user = User(username='foo', email='foo@bar.com')
+    user.save()
+    assert user.password is None
 
-        user = UserFactory(password='myPrecious')
-        db.session.commit()
-        assert bool(user.username)
-        assert bool(user.email)
-        assert bool(user.created_at)
-        assert user.is_admin is False
-        assert user.active is True
-        assert user.check_password('myPrecious')
 
-    def test_check_password(self):
-        """Check password."""
+@pytest.mark.usefixtures('db')
+def test_factory(db):
+    """Test user factory."""
 
-        user = User.create(username='foo', email='foo@bar.com',
-                           password='fooBarBaz123')
-        assert user.check_password('fooBarBaz123') is True
-        assert user.check_password('barFooBaz') is False
+    user = UserFactory(password='myPrecious')
+    db.session.commit()
+    assert bool(user.username)
+    assert bool(user.email)
+    assert bool(user.created_at)
+    assert user.is_admin is False
+    assert user.active is True
+    assert user.check_password('myPrecious')
 
-    def test_full_name(self):
-        """User full name."""
 
-        user = UserFactory(first_name='Foo', last_name='Bar')
-        assert user.full_name == 'Foo Bar'
+@pytest.mark.usefixtures('db')
+def test_check_password():
+    """Check password."""
 
-    def test_roles(self):
-        """Add a role to a user."""
+    user = User.create(username='foo', email='foo@bar.com',
+                       password='fooBarBaz123')
+    assert user.check_password('fooBarBaz123') is True
+    assert user.check_password('barFooBaz') is False
 
-        role = Role(name='admin')
-        role.save()
-        user = UserFactory()
-        user.roles.append(role)
-        user.save()
-        assert role in user.roles
+
+@pytest.mark.usefixtures('db')
+def test_full_name():
+    """User full name."""
+
+    user = UserFactory(first_name='Foo', last_name='Bar')
+    assert user.full_name == 'Foo Bar'
+
+
+@pytest.mark.usefixtures('db')
+def test_roles():
+    """Add a role to a user."""
+
+    role = Role(name='admin')
+    role.save()
+    user = UserFactory()
+    user.roles.append(role)
+    user.save()
+    assert role in user.roles
