becc047b86e61c506013c5b263a410a58be035a9
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-14 14:23:08 +0200

    Issue #11 Add RegisterForm unittests for collection

--- tests/forms/test_collection.py
+++ tests/forms/test_collection.py
@@ -3,31 +3,49 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from random import choice
+
 from xl_auth.collection.forms import RegisterForm
 
 
 # noinspection PyUnusedLocal
-def test_validate_without_full_name(db):
-    """Attempt registering with name shorter than 3 chars."""
-    form = RegisterForm(username='mr.librarian@kb.se', full_name='01',
-                        password='example', confirm='example')
+def test_register_form_validate_without_code(db):
+    """Attempt registering with code shorter than 2 chars."""
+    form = RegisterForm(code='1', friendly_name='The old books')
 
     assert form.validate() is False
-    assert 'Field must be between 3 and 255 characters long.' in form.full_name.errors
+    assert 'Field must be between 2 and 8 characters long.' in form.code.errors
 
 
-def test_validate_email_already_registered(user):
-    """Enter email that is already registered."""
-    form = RegisterForm(username=user.email, full_name='Another Name Perhaps',
-                        password='example', confirm='example')
+def test_register_form_validate_code_already_registered(collection):
+    """Enter code that is already registered."""
+    form = RegisterForm(code=collection.code, friendly_name='Shelf no 3')
 
     assert form.validate() is False
-    assert 'Email already registered' in form.username.errors
+    assert 'Code already registered' in form.code.errors
 
 
 # noinspection PyUnusedLocal
-def test_validate_success(db):
+def test_register_form_validate_without_friendly_name(db):
+    """Attempt registering with friendly_name shorter than 2 chars."""
+    form = RegisterForm(code='SFX', friendly_name='1')
+
+    assert form.validate() is False
+    assert 'Field must be between 2 and 255 characters long.' in form.friendly_name.errors
+
+
+# noinspection PyUnusedLocal
+def test_register_form_validate_unsupported_category(db):
+    """Attempt registering with custom/unsupported category."""
+    form = RegisterForm(code='SF2', friendly_name='Top shelf', category='Made-up by me')
+
+    assert form.validate() is False
+    assert "Value must be 'bibliography' or 'library'." in form.category.errors
+
+
+# noinspection PyUnusedLocal
+def test_register_form_validate_success(db):
     """Register with success."""
-    form = RegisterForm(username='first.last@kb.se', full_name='First Last',
-                        password='example', confirm='example')
+    form = RegisterForm(code='XZY', friendly_name='National Library, section D9, shelf 2, row 1',
+                        category=choice['bibliography', 'library'])
     assert form.validate() is True
