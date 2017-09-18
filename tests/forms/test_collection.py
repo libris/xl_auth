923c7cc68c98d2bf5e67d3fad42b881152b3bffa
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-18 11:35:47 +0200

    Issue #11 Update tests/forms/test_collection.py to test EditForm on par with RegisterForm

--- tests/forms/test_collection.py
+++ tests/forms/test_collection.py
@@ -5,20 +5,29 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from random import choice
 
-from xl_auth.collection.forms import RegisterForm
+from xl_auth.collection.forms import EditForm, RegisterForm
 
 
 # noinspection PyUnusedLocal
 def test_register_form_validate_without_code(db):
-    """Attempt registering with code shorter than 2 chars."""
+    """Attempt registering entry with code shorter than 2 chars."""
     form = RegisterForm(code='1', friendly_name='The old books', category='library')
 
     assert form.validate() is False
     assert 'Field must be between 2 and 8 characters long.' in form.code.errors
 
 
+# noinspection PyUnusedLocal
+def test_edit_form_validate_without_code(db):
+    """Attempt editing entry without a code."""
+    form = EditForm(friendly_name='The old books', category='library')
+
+    assert form.validate() is False
+    assert 'This field is required.' in form.code.errors
+
+
 def test_register_form_validate_code_already_registered(collection):
-    """Enter code that is already registered."""
+    """Attempt register code that is already registered."""
     form = RegisterForm(code=collection.code, friendly_name='Shelf no 3', category='uncategorized')
 
     assert form.validate() is False
@@ -26,9 +35,26 @@ def test_register_form_validate_code_already_registered(collection):
 
 
 # noinspection PyUnusedLocal
+def test_edit_form_validate_code_does_not_exist(db):
+    """Attempt to edit entry with code that is not registered."""
+    form = EditForm(code='missing', friendly_name='KB wing 3, shelf 1', category='library')
+
+    assert form.validate() is False
+    assert 'Code does not exist' in form.code.errors
+
+
+# noinspection PyUnusedLocal
 def test_register_form_validate_without_friendly_name(db):
-    """Attempt registering with friendly_name shorter than 2 chars."""
-    form = RegisterForm(code='SFX', friendly_name='1')
+    """Attempt registering entry with friendly_name shorter than 2 chars."""
+    form = RegisterForm(code='SFX', friendly_name='1', category='uncategorized')
+
+    assert form.validate() is False
+    assert 'Field must be between 2 and 255 characters long.' in form.friendly_name.errors
+
+
+def test_edit_form_validate_without_friendly_name(collection):
+    """Attempt editing entry with friendly_name shorter than 2 chars."""
+    form = EditForm(code=collection.code, friendly_name='0', category='uncategorized')
 
     assert form.validate() is False
     assert 'Field must be between 2 and 255 characters long.' in form.friendly_name.errors
@@ -36,8 +62,16 @@ def test_register_form_validate_without_friendly_name(db):
 
 # noinspection PyUnusedLocal
 def test_register_form_validate_unsupported_category(db):
-    """Attempt registering with custom/unsupported category."""
+    """Attempt registering entry with custom/unsupported category."""
     form = RegisterForm(code='SF2', friendly_name='Top shelf', category='Made-up by me')
+
+    assert form.validate() is False
+    assert 'Not a valid choice' in form.category.errors
+
+
+def test_edit_form_validate_unsupported_category(collection):
+    """Attempt editing entry with custom/unsupported category."""
+    form = EditForm(code=collection.code, friendly_name='Left shelf', category='Made-up by me')
 
     assert form.validate() is False
     assert 'Not a valid choice' in form.category.errors
@@ -45,7 +79,14 @@ def test_register_form_validate_unsupported_category(db):
 
 # noinspection PyUnusedLocal
 def test_register_form_validate_success(db):
-    """Register with success."""
+    """Register entry with success."""
     form = RegisterForm(code='XZY', friendly_name='National Library, section D9, shelf 2, row 1',
                         category=choice(['bibliography', 'library', 'uncategorized']))
+    assert form.validate() is True
+
+
+def test_edit_form_validate_success(collection):
+    """Edit entry with success."""
+    form = EditForm(code=collection.code, friendly_name='National Library, section D9, shelf 9',
+                    category=choice(['bibliography', 'library', 'uncategorized']))
     assert form.validate() is True
