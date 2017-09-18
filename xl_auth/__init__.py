95b7d1dcdafac8c6b257a12f2f022934da91dd8e
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-18 16:36:18 +0200

    Fix failing test

--- xl_auth/__init__.py
+++ xl_auth/__init__.py
@@ -3,10 +3,17 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import json
+from os import path
 
 __all__ = ['__name__', '__version__']
 
 
+def _read_package_json_version():
+    with open(path.join(path.dirname(__file__), '..', 'package.json')) as package_json:
+        return json.load(package_json)['version']
+
+
 __name__ = 'xl_auth'
 
-__version__ = '0.2.0'
+__version__ = _read_package_json_version()
