e6404abe08ffb9001b4b0e9ba5fb358536c64ed7
vtepliuk <victoria.tepliuk@gmail.com>
2017-09-27 16:51:05 +0200

    Issue #30 Update tests/forms/test_collection to allow 1-char collection codes

--- tests/forms/test_collection.py
+++ tests/forms/test_collection.py
@@ -12,11 +12,19 @@ from xl_auth.collection.forms import EditForm, RegisterForm
 
 # noinspection PyUnusedLocal
 def test_register_form_validate_without_code(db):
-    """Attempt registering entry with code shorter than 2 chars."""
-    form = RegisterForm(code='1', friendly_name='The old books', category='library')
+    """Attempt registering entry with empty string for code."""
+    form = RegisterForm(code='', friendly_name='The old books', category='library')
 
     assert form.validate() is False
-    assert _('Field must be between 2 and 8 characters long.') in form.code.errors
+    assert _('This field is required.') in form.code.errors
+
+
+def test_register_form_validate_with_too_long_code(db):
+    """Attempt registering entry with code longer than 5 chars."""
+    form = RegisterForm(code='123456', friendly_name='The hidden books', category='library')
+
+    assert form.validate() is False
+    assert _('Field must be between 1 and 5 characters long.') in form.code.errors
 
 
 def test_edit_form_validate_without_code(collection):
@@ -38,7 +46,7 @@ def test_register_form_validate_code_already_registered(collection):
 # noinspection PyUnusedLocal
 def test_edit_form_validate_code_does_not_exist(db):
     """Attempt to edit entry with code that is not registered."""
-    form = EditForm('missing', code='missing', friendly_name='KB wing 3', category='library')
+    form = EditForm('none', code='none', friendly_name='KB wing 3', category='library')
 
     assert form.validate() is False
     assert _('Code does not exist') in form.code.errors
