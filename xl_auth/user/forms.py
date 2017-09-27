a0f16dd82df35fcdc945a0b08308f52a9f9c59c7
vtepliuk <victoria.tepliuk@gmail.com>
2017-09-27 19:25:50 +0200

    Update form validation for user registration; now passing all tests

--- xl_auth/user/forms.py
+++ xl_auth/user/forms.py
@@ -37,7 +37,7 @@ class RegisterForm(FlaskForm):
         if not initial_validation:
             return False
 
-        user = User.query.filter_by(email=self.username.data).first()
+        user = User.query.filter(User.email.ilike(self.username.data)).first()
 
         if user:
             self.username.errors.append(_('Email already registered'))
