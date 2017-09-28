31c40fa10487f6647d1bfda6a90e78c5390b04d2
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-09-28 15:31:41 +0200

    Issue #27 Add Permission relationships to User and Collection model; now passing all tests

--- xl_auth/user/models.py
+++ xl_auth/user/models.py
@@ -17,7 +17,7 @@ class Role(SurrogatePK, Model):
     __tablename__ = 'roles'
     name = Column(db.String(80), unique=True, nullable=False)
     user_id = reference_col('users', nullable=True)
-    user = relationship('User', backref='roles')
+    user = relationship('User', back_populates='roles', uselist=False)
 
     def __init__(self, name, **kwargs):
         """Create instance."""
@@ -34,11 +34,12 @@ class User(UserMixin, SurrogatePK, Model):
     __tablename__ = 'users'
     email = Column(db.String(255), unique=True, nullable=False)
     full_name = Column(db.String(255), unique=False, nullable=False)
-    #: The hashed password
     password = Column(db.Binary(128), nullable=True)
-    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     active = Column(db.Boolean(), default=False)
     is_admin = Column(db.Boolean(), default=False)
+    permissions = relationship('Permission', back_populates='user')
+    roles = relationship('Role', back_populates='user')
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
 
     def __init__(self, email, full_name, password=None, **kwargs):
         """Create instance."""
