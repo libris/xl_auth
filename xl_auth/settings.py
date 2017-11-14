5bb8e685624b14802995860b463b6b13938a7f38
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-11-14 11:11:08 +0100

    #41 Update settings to include stuff for Flask-Mail

--- xl_auth/settings.py
+++ xl_auth/settings.py
@@ -11,6 +11,8 @@ from . import __author__, __name__, __version__
 class Config(object):
     """Base configuration."""
 
+    SERVER_NAME = os.environ.get('SERVER_NAME', 'localhost')
+    PREFERRED_URL_SCHEME = os.environ.get('PREFERRED_URL_SCHEME', 'http')
     APP_NAME = __name__
     APP_VERSION = __version__
     APP_AUTHOR = __author__
@@ -26,6 +28,9 @@ class Config(object):
     WEBPACK_MANIFEST_PATH = 'webpack/manifest.json'
     BABEL_DEFAULT_LOCALE = os.environ.get('BABEL_DEFAULT_LOCALE', 'sv')
     BABEL_DEFAULT_TIMEZONE = 'utc'
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@kb.se')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.kb.se')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '25'))
     OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = int(
         os.environ.get('OAUTH2_PROVIDER_TOKEN_EXPIRES_IN', 3600))
 
