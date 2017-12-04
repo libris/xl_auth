fa2959ccb33350ec45a7f14137d49626cc8db38b
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-12-04 12:30:07 +0100

    #112 Add `test_user_must_approve_tos_on_login_if_unset` to `end2end/test_public_logging_in`

--- tests/forms/test_user_approve_tos.py
+++ tests/forms/test_user_approve_tos.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from datetime import datetime, timedelta
 
 import pytest
+from flask import url_for
 from flask_babel import gettext as _
 
 from xl_auth.user.forms import ApproveToSForm
@@ -17,7 +18,7 @@ from ..factories import UserFactory
 def test_validate_success():
     """Approve ToS form with success."""
     user = UserFactory(tos_approved_at=None)
-    form = ApproveToSForm(user, tos_approved='y')
+    form = ApproveToSForm(user, tos_approved='y', next_redirect=url_for('user.profile'))
 
     assert form.validate() is True
 
@@ -26,7 +27,7 @@ def test_validate_success():
 def test_validate_when_tos_approved_is_not_y():
     """Attempt approving ToS with 'tos_approved' set to e.g. 'no'."""
     user = UserFactory(tos_approved_at=None)
-    form = ApproveToSForm(user, tos_approved='no')
+    form = ApproveToSForm(user, tos_approved='no', next_redirect=url_for('user.profile'))
 
     assert form.validate() is False
     assert _('Invalid option "%(value)s".', value='no') in form.tos_approved.errors
@@ -36,7 +37,7 @@ def test_validate_when_tos_approved_is_not_y():
 def test_validate_when_tos_approved_at_already_set():
     """Attempt approving ToS with 'tos_approved_at' already set."""
     user = UserFactory(tos_approved_at=datetime.utcnow() - timedelta(days=20))
-    form = ApproveToSForm(user)
+    form = ApproveToSForm(user, tos_approved='y', next_redirect=url_for('user.profile'))
 
     assert form.validate() is False
     assert _('ToS already approved at %(isoformat)s.',
