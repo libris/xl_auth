b392c7fa93b04bc2e28f225a6f90a16286fca4dd
Mats Blomdahl <mats.blomdahl@gmail.com>
2017-12-13 11:18:12 +0100

    #123 Add `tests/end2end/test_permission_deleting:test_cataloging_admin_can_delete_existing_permission` test case

--- tests/end2end/test_permission_deleting.py
+++ tests/end2end/test_permission_deleting.py
@@ -3,17 +3,16 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import re
-
 from flask import url_for
 from flask_babel import gettext as _
 
 from xl_auth.permission.models import Permission
 
+from ..factories import PermissionFactory
 
-# noinspection PyUnusedLocal
+
 def test_superuser_can_delete_existing_permission(superuser, permission, testapp):
-    """Delete existing permission."""
+    """Delete existing permission as superuser."""
     old_count = len(Permission.query.all())
     # Goes to homepage
     res = testapp.get('/')
@@ -28,8 +27,8 @@ def test_superuser_can_delete_existing_permission(superuser, permission, testapp
     # Clicks Delete button on a permission
     permission_user_email = permission.user.email
     permission_collection_code = permission.collection.code
-    res = res.click(href=url_for('permission.delete', permission_id=permission.id)
-                         + '\?next=' + url_for('permission.home'))
+    res = res.click(href=url_for('permission.delete', permission_id=permission.id) +
+                    '\?next=' + url_for('permission.home'))
     assert res.status_code == 200
     assert _('Acknowledge Deletion') in res
     assert _('Delete permissions for "%(username)s" on collection "%(code)s"?',
@@ -46,8 +45,47 @@ def test_superuser_can_delete_existing_permission(superuser, permission, testapp
     assert len(Permission.query.all()) == old_count - 1
 
 
+def test_cataloging_admin_can_delete_existing_permission(user, permission, superuser, testapp):
+    """Delete existing permission as cataloging admin for a collection."""
+    PermissionFactory(user=user, collection=permission.collection,
+                      cataloging_admin=True).save_as(superuser)
+    assert user.is_cataloging_admin_for(permission.collection) is True
+    old_count = len(Permission.query.all())
+    permission_user_email = permission.user.email
+    permission_collection_code = permission.collection.code
+    # Goes to homepage
+    res = testapp.get('/')
+    # Fills out login form
+    form = res.forms['loginForm']
+    form['username'] = user.email
+    form['password'] = 'myPrecious'
+    # Submits
+    res = form.submit().follow()
+
+    # We see no Permissions button
+    assert res.lxml.xpath("//a[contains(@text,'{0}')]".format(_('Permissions'))) == []
+
+    # Try to go there directly
+    testapp.get('/permissions/', status=403)
+
+    # Try to delete a specific permission directly
+    res = testapp.get(url_for('permission.delete', permission_id=permission.id))
+    form = res.forms['deletePermissionForm']
+    form['acknowledged'] = 'y'
+    res = form.submit()
+    assert res.status_code == 302
+    assert url_for('public.home') in res.location
+    res = res.follow()
+
+    # Permission was deleted, so number of permissions are 1 less than initial state
+    assert _('Successfully deleted permissions for "%(username)s" on collection "%(code)s".',
+             username=permission_user_email, code=permission_collection_code) in res
+    assert len(Permission.query.all()) == old_count - 1
+
+
 def test_user_cannot_delete_permission(user, permission, testapp):
-    """Attempt to delete a permission."""
+    """Attempt to delete a permission as non-cataloging admin user."""
+    assert user.is_cataloging_admin_for(permission.collection) is False
     old_count = len(Permission.query.all())
     # Goes to homepage
     res = testapp.get('/')
@@ -64,8 +102,12 @@ def test_user_cannot_delete_permission(user, permission, testapp):
     # Try to go there directly
     testapp.get('/permissions/', status=403)
 
-    # Try to delete
-    testapp.delete('/permissions/delete/1', status=403)
+    # Try to delete a specific permission directly
+    res = testapp.get(url_for('permission.delete', permission_id=permission.id))
+    form = res.forms['deletePermissionForm']
+    form['acknowledged'] = 'y'
+    res = form.submit()
+    assert _('You do not have sufficient privileges for this operation.') in res
 
     # Nothing was deleted
     assert len(Permission.query.all()) == old_count
