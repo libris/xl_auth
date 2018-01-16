6e96d8e116870bd5b12ee87be96530eba922f677
vtepliuk <victoria.tepliuk@gmail.com>
2018-01-16 17:35:32 +0100

    #180 Fix bind address, make 2/3-agnostic like other .py files, keep all Prod Run logic in gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
 """Gunicorn production config."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import multiprocessing
 
-bind = '127.0.0.1:5000'
+from click import echo
+
+bind = '0.0.0.0:5000'
+
 workers = multiprocessing.cpu_count() * 2 + 1
+
+
+def on_starting(_):
+    """Master process initializing."""
+    echo(_run_alembic_upgrade())
+
+
+def on_reload(_):
+    """Recycling workers due to SIGHUP."""
+    echo(_run_alembic_upgrade())
+
+
+def _run_alembic_upgrade():
+    from sh import Command
+    flask_cmd = Command('flask')
+    return flask_cmd('db', 'upgrade').stderr
