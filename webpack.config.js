e458042eb429e2a9ee8a31c4e60d2731a1d76a90
Lars Rosenström <lars.rosenstrom@kb.se>
2021-06-16 22:24:28 +0200

    Update webpack etc
    Upgrade webpack 0 -> 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
+const path = require('path');
 const webpack = require('webpack');
 
 /*
  * Webpack Plugins
  */
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // take debug mode from the environment
 const debug = (process.env.NODE_ENV !== 'production');
@@ -20,8 +20,9 @@ module.exports = {
   entry: {
     main_js: './assets/js/main',
     main_css: [
-      './node_modules/font-awesome/css/font-awesome.css',
-      './assets/css/style.less'
+      path.join(__dirname, 'node_modules', '@fortawesome', 'fontawesome-free', 'css', 'all.css'),
+      path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
+      path.join(__dirname, 'assets', 'css', 'style.less')
     ]
   },
   output: {
@@ -31,35 +32,50 @@ module.exports = {
     chunkFilename: '[id].[hash].js'
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css', 'less']
+    extensions: ['.js', '.jsx', '.css', '.less']
   },
   devtool: debug ? 'inline-sourcemap' : 'source-map',
   devServer: {
-    headers: { 'Access-Control-Allow-Origin': '*' }
+    headers: {'Access-Control-Allow-Origin': '*'}
   },
   module: {
-    loaders: [
-      { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.less$/,
-        loader: ExtractTextPlugin.extract({fallback: 'style-loader',
-                                           use: 'css-loader!less-loader' }) },
-      { test: /\.css$/,
-        loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader' }) },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg|png|jpe?g|gif|ico)(\?.*)?$/i,
-        loader: 'file-loader?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]' },
-      { test: /\.js$/, exclude: /node_modules/,
-        loader: 'babel-loader', query: { presets: ['es2015'], cacheDirectory: true } }
+     rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+            }
+          },
+            'css-loader',
+            'less-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+            }
+          },
+          'css-loader'
+        ]
+      },
+      {test: /\.html$/, loader: 'raw-loader'},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff'}},
+      {
+        test: /\.(ttf|eot|svg|png|jpe?g|gif|ico)(\?.*)?$/i,
+        loader: 'file-loader',
+        options: {context: rootAssetPath, name: '[path][name].[ext]'}
+      },
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', options: {presets: ['@babel/preset-env'], cacheDirectory: true}}
     ]
   },
   plugins: [
-    new ExtractTextPlugin('[name].[hash].css'),
-    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
-    new ManifestRevisionPlugin(__dirname + '/xl_auth/webpack/manifest.json', {
-      rootAssetPath,
-      ignorePaths: ['/js', '/css']
-    })
+    new MiniCssExtractPlugin({filename: '[name].bundle.css'}),
+    new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'})
   ].concat(debug ? [] : [
     // production webpack plugins go here
     new webpack.DefinePlugin({
