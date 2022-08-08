28d9ea3d3cba8311a5386f2359a87ea695c34834
Olov Ylinenpää <olov.ylinenpaa@kb.se>
2022-08-08 16:04:04 +0200

    Allow CORS for all domains using flask-cors
    The Access-Control-Allow-Origin header used to be set by nginx.
    This makes it work in a local dev environment as well.

--- autoapp.py
+++ autoapp.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from flask.helpers import get_debug_flag
+from flask_cors import CORS
 
 from xl_auth.app import create_app
 from xl_auth.settings import DevConfig, ProdConfig
@@ -11,3 +12,4 @@ from xl_auth.settings import DevConfig, ProdConfig
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
+CORS(app)
